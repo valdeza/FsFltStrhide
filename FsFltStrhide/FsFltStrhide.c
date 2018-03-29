d58ac0ba8770de1d3cb314ebe789c9e5b82e5690
valdeza <valdeza@users.noreply.github.com>
2018-03-29 00:55:28 -0400

    Project now in minimal compilable state.

--- FsFltStrhide/FsFltStrhide.c
+++ FsFltStrhide/FsFltStrhide.c
@@ -27,7 +27,7 @@ ULONG_PTR OperationStatusCtx = 1;
 #define PTDBG_TRACE_ROUTINES            0x00000001
 #define PTDBG_TRACE_OPERATION_STATUS    0x00000002
 
-ULONG gTraceFlags = 0;
+ULONG gTraceFlags = 0x3;
 
 
 #define PT_DBG_PRINT( _dbgLevel, _string )          \
